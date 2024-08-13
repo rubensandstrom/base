bc4ffb5f15c1e7e255330337d713a2695ed5db26
Ruben Sandström <rubensandstrom@gmail.com>
2024-08-13 23:13:41 +0200

    initial commit

---
+++ alloc.h
@@ -0,0 +1,4 @@
+typedef struct {
+  void *buffer;
+  void *next;
+} arena_t;
---
+++ int.h
@@ -0,0 +1,14 @@
+typedef signed char         s8;
+typedef signed short        s16;
+typedef signed int          s32;
+typedef signed long long    s64;
+typedef signed long long    ssize;
+typedef signed long long    smode;
+
+
+typedef unsigned char       u8;
+typedef unsigned short      u16;
+typedef unsigned int        u32;
+typedef unsigned long long  u64;
+typedef unsigned long long  usize;
+typedef unsigned long long  umode;
